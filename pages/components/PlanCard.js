68798d0f7b18b5cbbd8d1315184305eeda4af563
Anilk <anil2941985@gmail.com>
2025-11-29 18:11:02 +0530

    Create PlanCard.js

---
+++ pages/components/PlanCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function PlanCard({ title, subtitle, price, onBook }) {
+  return (
+    <div className="result-card">
+      <div className="r-left">
+        <div className="r-title">{title}</div>
+        <div className="r-sub">{subtitle}</div>
+      </div>
+
+      <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
+        <div className="r-price">₹{price}</div>
+        <button className="r-book" onClick={onBook}>
+          Book
+        </button>
+      </div>
+    </div>
+  );
+}
